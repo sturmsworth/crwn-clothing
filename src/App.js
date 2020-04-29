e2642e4357135c421289f35cdb47095f014d986f
Palmer Sturman <sturman.palmer@gmail.com>
2020-04-29 12:39:29 -0400

    added header, and addtional css

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
+import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
 import ShopPage from "./pages/ShopPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
-        {/* <Route path="/hats/:hatsId" component={HatsPage} /> */}
+        <Route path="/contact" component={ContactPage} />
       </Switch>
     </div>
   );
